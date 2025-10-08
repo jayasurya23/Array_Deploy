c136b8aed12935ff344dcf33149f021115ed5a2b
jayasurya23 <b.suryabhas@gmail.com>
2025-10-08 11:12:47 -0400

    startup

--- startup.sh
+++ startup.sh
@@ -1,2 +1,8 @@
 #!/bin/bash
-streamlit run streamlit_app.py --server.port 8000 --server.address 0.0.0.0
+# This script will start your Streamlit app and make it accessible
+
+# Activate the Python environment if needed (optional, based on your setup)
+# source /path/to/your/virtualenv/bin/activate
+
+# Run Streamlit app
+streamlit run streamlit_app.py --server.port 8501 --server.address 0.0.0.0
