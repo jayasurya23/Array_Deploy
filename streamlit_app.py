988d45d19afd8b7ff09286eac7c0d172fac86ae5
jayasurya23 <b.suryabhas@gmail.com>
2026-01-21 16:14:57 -0500

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -16,7 +16,7 @@ st.set_page_config(
     page_icon="☀️",
     layout="wide"
 )
-version="2.2"
+version="2.3"
 # Title and description
 st.title(f"☀️ Solar Pile Optimization Analysis V {version}")
 st.markdown("""
